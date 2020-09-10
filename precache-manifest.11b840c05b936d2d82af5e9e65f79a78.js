42d7ee4c0ba9a6180ddec74090565dba1e24a775
dbusteed <busteed.davis@gmail.com>
2020-09-09 19:45:07 -0600

    Updates

---
+++ precache-manifest.11b840c05b936d2d82af5e9e65f79a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e1e5239225d37d449492ff81a34248",
+    "url": "/time-tracker/index.html"
+  },
+  {
+    "revision": "1b700750af0759453c1d",
+    "url": "/time-tracker/static/css/main.6aeb069a.chunk.css"
+  },
+  {
+    "revision": "4760daa6e7f8054d6300",
+    "url": "/time-tracker/static/js/2.c5da691c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/time-tracker/static/js/2.c5da691c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b700750af0759453c1d",
+    "url": "/time-tracker/static/js/main.83b69059.chunk.js"
+  },
+  {
+    "revision": "7f0ff96a53d0e16b23de",
+    "url": "/time-tracker/static/js/runtime-main.0767e1ea.js"
+  }
+]);
